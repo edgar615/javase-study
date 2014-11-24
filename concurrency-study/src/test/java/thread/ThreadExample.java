90182d3233bae8cdaa62d774ff6c44b55e7673bf
Edgar Zhang <edgar615@gmail.com>
2014-11-24 13:31:33 +0800

    concurrency-study

---
+++ concurrency-study/src/test/java/thread/ThreadExample.java
@@ -0,0 +1,14 @@
+package thread;
+
+public class ThreadExample {
+  public static void main(String[] args){
+     System.out.println(Thread.currentThread().getName());
+      for(int i=0; i<10; i++){
+         new Thread("" + i){
+            public void run(){
+             System.out.println("Thread: " + getName() + "running");
+            }
+         }.start();
+      }
+  }
+}
