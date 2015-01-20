4b932b4b650fd610ec44c8139594bbfaa8e424de
Edgar Zhang <edgar615@gmail.com>
2015-01-20 17:29:53 +0800

    Functions

--- guava-study/src/test/java/base/FunctionTest.java
+++ guava-study/src/test/java/base/FunctionTest.java
@@ -1,18 +1,15 @@
 package base;
 
-import com.google.common.base.CharMatcher;
-import com.google.common.base.Function;
-import com.google.common.base.Functions;
-import com.google.common.base.Predicate;
+import com.google.common.base.*;
 import com.google.common.collect.HashMultiset;
+import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Iterables;
 import com.google.common.collect.Multiset;
+import org.junit.Assert;
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.regex.Pattern;
 
 
 /**
@@ -75,5 +72,60 @@ public class FunctionTest {
         System.out.println(value);
         value = Functions.forMap(map).apply("email");
         //throw java.lang.IllegalArgumentException
+    }
+
+    @Test
+    public void testIdentity() {
+        Object obj = new Object();
+        Object value = Functions.identity().apply(obj);
+        Assert.assertSame(obj, value);
+    }
+
+    @Test
+    public void testToString() {
+        Set<Integer> set = ImmutableSet.of(1, 2);
+        System.out.println(Functions.toStringFunction().apply(set));
+        System.out.println(Functions.toStringFunction().apply(ImmutableSet.of(2, 1)));
+    }
+
+    @Test
+    public void testConstant() {
+        Set<Integer> set = ImmutableSet.of(1, 2);
+        System.out.println(Functions.constant("CONST VALUE").apply(set));
+        System.out.println(Functions.constant("CONST VALUE").apply(ImmutableSet.of(2, 1)));
+    }
+
+    @Test
+    public void testCompose() {
+        Set<Integer> set = ImmutableSet.of(1, 2);
+        System.out.println(Functions.compose(Functions.constant("CONST VALUE"), Functions.toStringFunction()).apply(set));
+    }
+
+    @Test
+    public void testPredicate() {
+        Predicate<Integer> predicate = new Predicate<Integer>() {
+            @Override
+            public boolean apply(Integer input) {
+                return input < 0;
+            }
+        };
+        Assert.assertFalse(predicate.apply(1));
+    }
+
+    @Test
+    public void testInstanceOf() {
+        HashMap map = new HashMap();
+        Assert.assertTrue(Predicates.instanceOf(Map.class).apply(map));
+    }
+
+    @Test
+    public void testAssignableFrom() {
+        Assert.assertTrue(Predicates.assignableFrom(Map.class).apply(HashMap.class));
+    }
+
+    @Test
+    public void testContains() {
+        Assert.assertTrue(Predicates.contains(Pattern.compile("[0-3]{3}")).apply("123"));
+        Assert.assertFalse(Predicates.contains(Pattern.compile("[0-3]{3}")).apply("34"));
     }
 }
