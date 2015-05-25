85564e6cc8d021cee32257603f94e52ba18b33b5
Edgar Zhang <edgar615@gmail.com>
2015-05-25 23:37:29 +0800

    copy file zip仍然有问题

--- commons-vfs2-study/src/test/java/FileObjectTest.java
+++ commons-vfs2-study/src/test/java/FileObjectTest.java
@@ -1,8 +1,11 @@
-import org.apache.commons.vfs2.FileObject;
-import org.apache.commons.vfs2.FileSystemException;
-import org.apache.commons.vfs2.FileSystemManager;
-import org.apache.commons.vfs2.VFS;
+import org.apache.commons.vfs2.*;
+import org.apache.commons.vfs2.provider.zip.ZipFileObject;
 import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 /**
  * Created by Administrator on 2015/5/25.
@@ -10,15 +13,42 @@ import org.junit.Test;
 public class FileObjectTest {
 
     @Test
-    public void testFileObject() throws FileSystemException {
+    public void testFileObject() throws IOException {
         FileSystemManager fileSystemManager = VFS.getManager();
         //Each file is represented by a FileObject instance. Using this interface you can create or delete the file, list its children, read or write its content, and so on
-        FileObject jarFile = fileSystemManager.resolveFile("jar:f://commons-vfs2-examples-2.0-sources.jar");
+        FileObject jarFile = fileSystemManager.resolveFile("file:///home/edgar/dev/test/TarenaBaiWenBaiDa.pdf");
+//        System.out.println(jarFile.getClass());
+//        System.out.println(jarFile.getType());
+//        FileObject[] children = jarFile.getChildren();
+//        System.out.println("Children of " + jarFile.getName().getURI());
+//        for (FileObject fo : children) {
+//            System.out.println(fo.getName().getBaseName());
+//        }
 
-        FileObject[] children = jarFile.getChildren();
-        System.out.println("Children of " + jarFile.getName().getURI());
-        for (FileObject fo : children) {
-            System.out.println(fo.getName().getBaseName());
-        }
+        FileObject destFile = fileSystemManager.resolveFile("file:///home/edgar/dev/vfs/a");
+        destFile.createFile();
+//        ZipOutputStream zos = new ZipOutputStream(destFile.getContent().getOutputStream());
+//        // add entry/-ies.
+//        ZipEntry zipEntry = new ZipEntry("name_inside_zip");
+//        InputStream is = jarFile.getContent().getInputStream();
+//
+//// Write to zip.
+//        byte[] buf = new byte[1024];
+//        zos.putNextEntry(zipEntry);
+//        for (int readNum; (readNum = is.read(buf)) != -1;) {
+//            zos.write(buf, 0, readNum);
+//        }
+
+//        destFile.createFolder();
+//        System.out.println(destFile.getType());
+//        if (destFile.exists()) {
+//            System.out.println(destFile.getName().getURI());
+//        } else {
+//            destFile.createFile();
+//            destFile.copyFrom(jarFile, Selectors.SELECT_SELF);
+//        }
+//        System.out.println(destFile.getType());
+        destFile.copyFrom(jarFile, Selectors.SELECT_SELF);
+//        jarFile.moveTo(destFile);
     }
 }
