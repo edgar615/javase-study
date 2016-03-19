8fb0d1f27f0ff3da5acf7349a69908ff4ed13127
Edgar Zhang <edgar615@gmail.com>
2016-03-19 10:06:10 +0800

    删除vert.x

--- vertx-simple/src/main/java/com/edgar/vertx/eventbus/MyPojo.java
+++
@@ -1,7 +0,0 @@
-package com.edgar.vertx.eventbus;
-
-/**
- * Created by Administrator on 2015/8/31.
- */
-public class MyPojo {
-}
--- vertx-simple/src/main/resources/products.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS PRODUCT;
-CREATE TABLE PRODUCT(
-id INTEGER,
-description VARCHAR(1000)
-);
-INSERT INTO PRODUCT (id, description) VALUES (1, 'product 1');
-INSERT INTO PRODUCT (id, description) VALUES (2, 'product 2');
-INSERT INTO PRODUCT (id, description) VALUES (1, 'product 3');
-INSERT INTO PRODUCT (id, description) VALUES (4, 'product 4');
